a9bb12d88996cd274d7e06dd5851082cea3dac28
Mateusz Kapeluszny <mkapeluszny@gmail.com>
2019-03-07 12:14:32 +0100

    init commit

---
+++ src/config_sample.js
@@ -0,0 +1,12 @@
+export default {
+	apiGateway: {
+		REGION: 'eu-west-1',
+		URL: 'https://4pyvzyzov7.execute-api.eu-west-1.amazonaws.com/dev'
+	},
+	cognito: {
+		REGION: 'eu-west-1',
+		USER_POOL_ID: 'eu-west-1_h6GvHrDaF',
+		APP_CLIENT_ID: '5f985si3cul9ucb9g3aof3v8of',
+		IDENTITY_POOL_ID: 'eu-west-1:2baca029-ff3b-45cf-a897-56e49c87b44b'
+	}
+};
